186965889e456d934b7d2952ddad3bfb39e8f277
Felix Lindgren <felix.p.lindgren@gmail.com>
2024-12-11 11:24:24 +0100

    Refactor evolution logic and update input handling for Day 11

--- 11/main.py
+++ 11/main.py
@@ -3,52 +3,43 @@ from functools import cache
 input = open('input.txt').readline().strip().split(" ")
 input = list(map(int, input))
 
-def apply_rules(stone):
+@cache
+def evolve(stone, blinks):
+    # If we have no more blinks, we return 1 because we're only interested in the number of stones
+    if blinks == 0:
+        return 1
+
     # First rule
     if stone == 0:
-        return [1]
+        return evolve(1, blinks - 1)
     
-    str_stone = str(stone)
     # Second rule
+    str_stone = str(stone)
     if len(str_stone) % 2 == 0:
         half = len(str_stone) // 2
-        return [int(str_stone[:half]), int(str_stone[half:])]
+        return evolve(int(str_stone[:half]), blinks - 1) + evolve(int(str_stone[half:]), blinks - 1)
     
     # Third rule
-    return [stone * 2024]
-
-@cache
-def fast_forward(stone, steps):
-    new_stones = apply_rules(stone)
-
-    if steps == 1:
-        return len(new_stones)
-    else:
-        total_of_stones = 0
-
-        for new_stone in new_stones:
-            total_of_stones += fast_forward(new_stone, steps - 1)
-
-        return total_of_stones
-
+    return evolve(stone * 2024, blinks - 1)
 
 def part_1():
-    total_of_stones = 0
+    amount_of_stones = 0
 
     for stone in input:
-        total_of_stones += fast_forward(stone, 25)
+        amount_of_stones += evolve(stone, 25)
 
-    return total_of_stones
-
+    return amount_of_stones
 
 def part_2():
-    total_of_stones = 0
+    amount_of_stones = 0
 
     for stone in input:
-        total_of_stones += fast_forward(stone, 75)
+        amount_of_stones += evolve(stone, 75)
 
-    return total_of_stones
+    return amount_of_stones
 
 if __name__ == "__main__":
     print(part_1())
     print(part_2())
+
+    
